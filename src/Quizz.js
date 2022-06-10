c0de9c98e11713249883126d73b83542e1cd7634
webiovg <kevin.kowalsky1@gmail.com>
2022-06-10 12:05:44 +0200

    done: ending display; to be done: imcrement score

--- src/Quizz.js
+++ src/Quizz.js
@@ -6,29 +6,41 @@ class Quizz extends React.Component {
         super(props)
         this.state = {
             arrayQuizz: this.props.quizz,
-            selectorQuestion: 0
+            selectorQuestion: 0,
+            score: 0
         }
     }
 
     implementSelectorQuestion = () => {
-        this.setState({ selectorQuestion: this.state.selectorQuestion+1 })
+        this.setState({ selectorQuestion: this.state.selectorQuestion + 1 })
     }
 
     render() {
         return (
             <div className="container-quizz">
-                <div className="quizz-counter">
-                    Question: { this.state.selectorQuestion + 1 }/3
-                </div>
+                {this.state.selectorQuestion < 3
+                    ?
+                    <div>
+                        <div className="quizz-counter">Question: {this.state.selectorQuestion + 1}/3</div>
 
-                <h2 className="quizz-title">{this.state.arrayQuizz[this.state.selectorQuestion].question}</h2>
-                
-                {this.state.arrayQuizz[this.state.selectorQuestion].answers.map(answer => 
-                    <div onClick={this.implementSelectorQuestion} className="quizz-answer">
-                        {answer}
+                        <h2 className="quizz-title">{this.state.arrayQuizz[this.state.selectorQuestion].question}</h2>
+
+                        {this.state.arrayQuizz[this.state.selectorQuestion].answers.map(answer =>
+                            <div onClick={this.implementSelectorQuestion} className="quizz-answer">
+                                {answer}
+                            </div>
+                        )}
                     </div>
-                )}
+
+                    :
+                    <div className="container-ending">
+                        <h2 className="container-ending-title">Votre score est {this.state.score}/3 !</h2>
+                        <button className="container-ending-button">Recommencer</button>
+                    </div>
+                }
+
             </div>
+
         )
     }
 }
