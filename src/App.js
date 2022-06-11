27fc4ba6db88f9e95b8ef0f9e9c0b51592c23597
webiovg <kevin.kowalsky1@gmail.com>
2022-06-11 12:27:04 +0200

    done: select three random questions in the API for the quizz

--- src/App.js
+++ src/App.js
@@ -15,13 +15,44 @@ class App extends React.Component {
     }
   }
 
-  // Load the API and store it
+  // Load the API, get 3 questions and store them
   componentDidMount() {
     axios.get('http://localhost:3001/questions').then(response => {
+      
+      // Get three random indexes
+      let indexes = this.getRandom(response.data)
+
       setTimeout(() => {
-        this.setState({ arrayQuizz: response.data, loading: false })
+
+        // Add three questions to the quizz
+        for (let i = 0; i < 3; i++) {
+          this.state.arrayQuizz.push(response.data[indexes[i]])
+        }
+
+        // End the loading screen
+        this.setState({ loading: false })
       }, 450);  
     })
+  }
+
+  // METHOD: Generate three random numbers
+  getRandom = (array) => {
+    let random1, random2, random3, resultArray = []
+
+    // Generate the three different random indexes
+    random1 = Math.floor(Math.random() * array.length)
+    do {
+      random2 = Math.floor(Math.random() * array.length)
+    } while (random2 === random1)
+    do {
+      random3 = Math.floor(Math.random() * array.length)
+    } while (random3 === random1 || random3 === random2)
+
+    resultArray.push(random1)
+    resultArray.push(random2)
+    resultArray.push(random3)
+
+    return resultArray
   }
   
   // RENDER
@@ -39,7 +70,8 @@ class App extends React.Component {
     // Quizz page
     return (
       <div className="container">
-        <Quizz quizz={this.state.arrayQuizz} />
+        {console.log(this.state.arrayQuizz)}
+        <Quizz quizz={this.state.arrayQuizz} retry={this.componentDidMount} />
       </div>
     )
   }
